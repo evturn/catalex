f5f63ad5e9418bb8c22e0f8ee6428259e74331d0
Evan Turner <evturn@gmail.com>
2016-01-03 09:53:29 -0500

    Split webpack environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
+  //
+  // Ain't no reason for this in development
+  //
+  // output: {
+  //   path: PATHS.build,
+  //   filename: 'bundle.js'
+  // },
+  //
   module: {
     loaders: [
       {
@@ -22,19 +28,27 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    stats: 'errors-only',
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Cache Money Records'
     })
   ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
