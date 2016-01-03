b329f0bea1efa2fb4aab2546f367c1e5daf25bdf
Evan Turner <evturn@gmail.com>
2016-01-03 11:41:51 -0500

    Create render function for App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import uuid from 'node-uuid';
+import React, {Component} from 'react';
 import Note from './Note.jsx';
 
-export default () => {
-  return <Note />;
-};
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Stay under a bed and when someone walks by grab their foot.'
+  },{
+    id: uuid.v4(),
+    task: 'Go to public restroom and rest publicly.'
+  },{
+    id: uuid.v4(),
+    task: 'No.'
+  }
+];
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+  renderNote(note) {
+    return(
+      <li key={note.id}>
+        <Note task={note.task} />
+      </li>
+    );
+  }
+}
