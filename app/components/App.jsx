5da390d0f5b873898d08864720e3d4478307193d
Evan Turner <evturn@gmail.com>
2016-01-03 13:40:26 -0500

    Set new state after editing note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -39,7 +39,15 @@ export default class App extends Component {
       }])
     });
   }
-  editNote(noteId, task) {
-    console.log('Pretending you can edit this.', noteId, task);
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if (note.id === id) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
   }
 }
