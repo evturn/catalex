6337fe091067ae5981f71c883de0098f51f02635
Evan Turner <evturn@gmail.com>
2016-01-03 12:02:19 -0500

    Move data into state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,21 +2,27 @@ import uuid from 'node-uuid';
 import React, {Component} from 'react';
 import Notes from './Notes';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Stay under a bed and when someone walks by grab their foot.'
-  },{
-    id: uuid.v4(),
-    task: 'Go to public restroom and rest publicly.'
-  },{
-    id: uuid.v4(),
-    task: 'No.'
-  }
-];
-
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Stay under a bed and when someone walks by grab their foot.'
+        },{
+          id: uuid.v4(),
+          task: 'Go to public restroom and rest publicly.'
+        },{
+          id: uuid.v4(),
+          task: 'No.'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
         <h1>The OoGhiJ MIQtxxXA is a super computer</h1>
