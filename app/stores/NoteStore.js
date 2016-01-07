237aa9e715cc437eb0af7a68c33bf6aecf951aca
Evan Turner <evturn@gmail.com>
2016-01-06 21:31:29 -0500

    Create public getter for NoteStore

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -8,6 +8,15 @@ class NoteStore {
     this.bindActions(NoteActions);
 
     this.notes = [];
+
+    this.exportPublicMethods({
+      get: this.get.bind(this)
+    });
+  }
+  get(ids) {
+    return (ids || []).map(
+      (id) => this.notes.filter((note) => note.id === id)
+    ).filter((a) => a.length).map((a) => a[0]);
   }
   create(note) {
     const notes = this.notes;
