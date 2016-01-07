18bd182192f288aa73561ce25f923065b52becf6
Evan Turner <evturn@gmail.com>
2016-01-06 20:40:10 -0500

    Create Lanes component

---
+++ app/components/Lanes.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Lanes extends Component {
+  render() {
+    return (
+      <div className="lanes">
+        I am lanes.
+      </div>
+    );
+  }
+}
