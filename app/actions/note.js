823bb500e60b5942d9d0d9861a5ba3fdb6b36a85
Evan Turner <evturn@gmail.com>
2016-02-29 16:27:51 -0500

    Create note actions

---
+++ app/actions/note.js
@@ -0,0 +1,17 @@
+export const createNote() => {
+  return {
+    type: 'CREATE_NOTE'
+  };
+};
+
+export const updateNote() => {
+  return {
+    type: 'UPDATE_NOTE'
+  };
+};
+
+export const deleteNote() => {
+  return {
+    type: 'DELETE_NOTE'
+  };
+};
