4f9f7ba7bf510bd2788fa72f0e3c2216da1634f7
Evan Turner <evturn@gmail.com>
2016-03-02 14:19:04 -0500

    Update store

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import store from './stores';
+import configureStore from './stores';
+import storage from './libs/storage';
+
+const APP_STORAGE = 'dollarz';
+const store = configureStore(storage.get(APP_STORAGE) || {});
+
+store.subscribe(() => {
+  if(!storage.get('debug')) {
+    storage.set(APP_STORAGE, store.getState());
+  }
+});
 
 render(
   <Provider store={store}>
--- app/stores/index.js
+++ app/stores/index.js
@@ -1,35 +1,6 @@
 import { createStore } from 'redux';
-import reducer from '../reducers';
+import rootReducer from '../reducers';
 
-const storage = {
-  get(key) {
-    try {
-      return JSON.parse(localStorage.getItem(key));
-    } catch(err) {
-      return null;
-    }
-  },
-  set(key, val) {
-    localStorage.setItem(key, JSON.stringify(val));
-  }
-};
-
-const getInitialState = (storeName) => {
-  let data;
-
-  try {
-    data = storage.get(storeName);
-  } catch(err) {
-    console.error('Failed to bootstrap data. I am also sleeping with your wife.', err);
-  }
-
-  return {
-    note: data,
-    lane: data
-  };
-};
-
-const initialState = getInitialState('app');
-const store = createStore(reducer);
-
-export default store;
+export default function configureStore(initialState) {
+  return createStore(rootReducer, initialState);
+}
